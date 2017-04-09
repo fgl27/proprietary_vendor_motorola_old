35740f97f3971bbcdcb0a858771147cfb43f7544
Felipe Leon <fglfgl27@gmail.com>
2017-04-09 10:43:41 -0300

    Quark: add permission to my apps
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- quark/quark-vendor.mk
+++ quark/quark-vendor.mk
@@ -389,7 +389,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/quark/proprietary/app/KernelAdiutor/KernelAdiutor.apk:system/app/KernelAdiutor/KernelAdiutor.apk \
     vendor/motorola/quark/proprietary/app/InductiveChargeUI/InductiveChargeUI.apk:system/app/InductiveChargeUI/InductiveChargeUI.apk \
     vendor/motorola/quark/proprietary/etc/permissions/com.verizon.hardware.telephony.srlte.xml:system/etc/permissions/com.verizon.hardware.telephony.srlte.xml \
-    vendor/motorola/quark/proprietary/app/substratum/substratum.apk:system/app/substratum/substratum.apk
+    vendor/motorola/quark/proprietary/app/substratum/substratum.apk:system/app/substratum/substratum.apk \
+    vendor/motorola/quark/proprietary/etc/permissions/bhb27.xml:system/etc/permissions/bhb27.xml
 
 # Verizon IMS from Shamu current config doesn’t work for quark
 #$(call inherit-product, vendor/motorola/quark/quark-vendor-ims-blobs.mk)
