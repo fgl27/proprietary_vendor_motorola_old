e13084333e963dd32398260558c6758fdd2e0753
Felipe Leon <fglfgl27@gmail.com>
2018-07-16 14:20:30 -0300

    Quark: move etc/diag_mdlog to vendor
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- quark/quark-vendor-ims-blobs.mk
+++ quark/quark-vendor-ims-blobs.mk
@@ -68,11 +68,11 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/quark/proprietary/vendor/lib/lib-imsrcscmservice.so:system/vendor/lib/lib-imsrcscmservice.so \
     vendor/motorola/quark/proprietary/lib/libmsp_vzw.so:system/lib/libmsp_vzw.so \
     vendor/motorola/quark/proprietary/priv-app/LteBroadcastServices_vzw/lib/arm/libmsp_vzw.so:system/priv-app/LteBroadcastServices_vzw/lib/arm/libmsp_vzw.so \
-    vendor/motorola/quark/proprietary/etc/diag_mdlog/Connection_Issues_V11.cfg:system/etc/diag_mdlog/Connection_Issues_V11.cfg \
-    vendor/motorola/quark/proprietary/etc/diag_mdlog/default.cfg:system/etc/diag_mdlog/default.cfg \
-    vendor/motorola/quark/proprietary/etc/diag_mdlog/default.opts:system/etc/diag_mdlog/default.opts \
-    vendor/motorola/quark/proprietary/etc/diag_mdlog/FIT_V20_1.cfg:system/etc/diag_mdlog/FIT_V20_1.cfg \
-    vendor/motorola/quark/proprietary/etc/diag_mdlog/FIT_V20_2_volte_full_ip.cfg:system/etc/diag_mdlog/FIT_V20_2_volte_full_ip.cfg \
-    vendor/motorola/quark/proprietary/etc/diag_mdlog/Vzw_Connection_Issues_V2.cfg:system/etc/diag_mdlog/Vzw_Connection_Issues_V2.cfg \
+    vendor/motorola/quark/proprietary/vendor/etc/diag_mdlog/Connection_Issues_V11.cfg:system/vendor/etc/diag_mdlog/Connection_Issues_V11.cfg \
+    vendor/motorola/quark/proprietary/vendor/etc/diag_mdlog/default.cfg:system/vendor/etc/diag_mdlog/default.cfg \
+    vendor/motorola/quark/proprietary/vendor/etc/diag_mdlog/default.opts:system/vendor/etc/diag_mdlog/default.opts \
+    vendor/motorola/quark/proprietary/vendor/etc/diag_mdlog/FIT_V20_1.cfg:system/vendor/etc/diag_mdlog/FIT_V20_1.cfg \
+    vendor/motorola/quark/proprietary/vendor/etc/diag_mdlog/FIT_V20_2_volte_full_ip.cfg:system/vendor/etc/diag_mdlog/FIT_V20_2_volte_full_ip.cfg \
+    vendor/motorola/quark/proprietary/vendor/etc/diag_mdlog/Vzw_Connection_Issues_V2.cfg:system/vendor/etc/diag_mdlog/Vzw_Connection_Issues_V2.cfg \
     vendor/motorola/quark/proprietary/framework/com.qualcomm.location.vzw_library.jar:system/framework/com.qualcomm.location.vzw_library.jar \
     vendor/motorola/quark/proprietary/etc/permissions/com.qualcomm.location.vzw_library.xml:system/etc/permissions/com.qualcomm.location.vzw_library.xml
