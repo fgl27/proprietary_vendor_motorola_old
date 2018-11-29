f19324f547965d88cfe63ee84a4dca6e7c2789a5
Felipe Leon <fglfgl27@gmail.com>
2018-11-29 17:53:23 -0200

    Quark: move mcamera to system
    to allow DND
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- quark/Android.mk
+++ quark/Android.mk
@@ -432,12 +432,11 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := mcamera
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/vendor/app/mcamera/mcamera.apk
+LOCAL_SRC_FILES := proprietary/app/mcamera/mcamera.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 endif
